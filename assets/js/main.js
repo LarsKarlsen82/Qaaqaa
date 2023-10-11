934b4d5bf1c5a1c354dedcdbeb46106135daf3b1
LarsKarlsen82 <141730453+LarsKarlsen82@users.noreply.github.com>
2023-10-11 16:40:41 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,8 @@
 // main.js
-import { fetchWeatherData } from './assets/js/module/weatherApi.js';
-import { updateWeatherUI } from './assets/js/weatherDisplay.js';
+//import { fetchWeatherData } from './assets/js/module/weatherApi.js';
+//import { updateWeatherUI } from './assets/js/weatherDisplay.js';
+import { fetchWeatherData } from '/module/weatherApi.js';
+//import { updateWeatherUI } from 'weatherDisplay.js';
 
 const searchBox = document.querySelector('.search input');
 const searchBtn = document.querySelector('.search button');
