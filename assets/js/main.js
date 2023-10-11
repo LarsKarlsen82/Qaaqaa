44722336335eca43cc3b592529fac6d49c4e53eb
LarsKarlsen82 <141730453+LarsKarlsen82@users.noreply.github.com>
2023-10-11 19:42:30 +0200

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,5 +31,4 @@ searchBtn.addEventListener('click', async () => {
         document.querySelector('.error').style.display = 'block';
         document.querySelector('.weather').style.display = 'none';
     }
-    console.info('Vakaavaat');
 });
